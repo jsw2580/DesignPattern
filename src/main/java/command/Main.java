2a66ef57b63f2fb96e373960553b81e8629b43d7
jsw <1121080049@qq.com>
2021-06-30 00:02:52 +0800

    Command命令

---
+++ src/main/java/command/Main.java
@@ -0,0 +1,22 @@
+package command;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Content content = new Content();
+
+        Command insertCommand = new InsertCommand(content);
+        insertCommand.doit();
+        insertCommand.undo();
+
+        Command copyCommand = new CopyCommand(content);
+        copyCommand.doit();
+        copyCommand.undo();
+
+        Command deleteCommand = new DeleteCommand(content);
+        deleteCommand.doit();
+        deleteCommand.undo();
+
+        System.out.println(content.msg);
+    }
+}
